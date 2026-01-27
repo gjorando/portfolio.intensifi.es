10d014782f349b0752237b7a34e6ae5f449d62a3
gjorando <guillaume.jorandon.1@ulaval.ca>
2026-01-27 11:44:16 +0100

    Updated to Tailwind

--- content/pages/lander.rst
+++ content/pages/lander.rst
@@ -18,15 +18,13 @@ Qui suis-je?
 
 .. projects::
 
-    .. project::
-        :title: mandelbrot
+    .. project:: mandelbrot
         :image: images/projects/mandelbrot.png
         :links: GitHub <https://github.com/gjorando/mandelbrot>
 
         Un générateur de fractales de `Mandelbrot <https://fr.wikipedia.org/wiki/Ensemble_de_Mandelbrot>`__, de `Julia <https://fr.wikipedia.org/wiki/Ensemble_de_Julia>`__ et de `Buddhabrot <https://fr.wikipedia.org/wiki/Buddhabrot>`__, codé en C++ avec libpng.
 
-    .. project::
-        :title: neurartist
+    .. project:: neurartist
         :image: images/projects/style-transfer.png
         :links: GitHub <https://github.com/gjorando/style-transfer>
 
@@ -39,15 +37,13 @@ Qui suis-je?
         * L\. A. Gatys, A. S. Ecker and M. Bethge, "Image Style Transfer Using Convolutional Neural Networks," 2016 IEEE Conference on Computer Vision and Pattern Recognition (CVPR), Las Vegas, NV, 2016, pp. 2414-2423, doi: `10.1109/CVPR.2016.265 <https://doi.org/10.1109/CVPR.2016.265>`__.
         * L\. A. Gatys, A. S. Ecker, M. Bethge, A. Hertzmann and E. Shechtman, "Controlling Perceptual Factors in Neural Style Transfer," 2017 IEEE Conference on Computer Vision and Pattern Recognition (CVPR), Honolulu, HI, 2017, pp. 3730-3738, doi: `10.1109/CVPR.2017.397 <https://doi.org/10.1109/CVPR.2017.397>`__.
 
-    .. project::
-        :title: Statues! & more!
+    .. project:: Statues! & more!
         :image: images/projects/minecraft-mod.png
         :links: GitHub <https://github.com/gjorando/statues-and-more>
 
         Un mod Minecraft, créé quand j'avais 15 ans, et qui a connu à l'époque un certain succès. Il a été le premier projet que j'ai mené de sa conception jusqu'à sa distribution, utilisant des liens de téléchargement monétisés pour récolter les fruits de mon travail.
 
-    .. project::
-        :title: Portal2.js
+    .. project:: Portal2.js
         :image: images/projects/portal-js.png
         :links: Site web <https://glados.intensifi.es/>, GitHub <https://github.com/gjorando/portal2.js>
 
--- pelicanconf.py
+++ pelicanconf.py
@@ -10,13 +10,64 @@ JINJA_ENVIRONMENT = {
     "extensions": ["jinja2.ext.do", "jinja2.ext.i18n"]
 }
 
+# Plugins
 PLUGIN_PATHS = ["plugin/pelican/plugins/"]
 PLUGINS = ["pelican_renn_plugin", "i18n_subsites"]
+
+# Extract some more metadata about the source file path, ie. the full path, directory, file name, and file name without the extension
+PATH_METADATA = r"(?P<full_path>(?P<directory_path>.*)\/(?P<filename_full>(?P<filename_no_ext>.*)\..*))"
 
 ## URLs ##
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
+
+# /index.html is redefined as the lander page by the special page lander_enable.rst
+# So the article list is set under posts/
+INDEX_URL = "posts/"
+INDEX_SAVE_AS = INDEX_URL + "index.html"
+
+# Archives page is accessible under archives/
+ARCHIVES_URL = "archives/"
+ARCHIVES_SAVE_AS = ARCHIVES_URL + "index.html"
+
+# Period archives are accessible under archives/{yyyy}/ and archives/{yyyy}/{mm}/
+YEAR_ARCHIVE_URL = "archives/{date:%Y}/"
+YEAR_ARCHIVE_SAVE_AS = YEAR_ARCHIVE_URL + "index.html"
+MONTH_ARCHIVE_URL = "archives/{date:%Y}/{date:%m}/"
+MONTH_ARCHIVE_SAVE_AS = MONTH_ARCHIVE_URL + "index.html"
+
+# Authors page is accessible under authors/
+AUTHORS_URL = "authors/"
+AUTHORS_SAVE_AS = "authors/index.html"
+
+# Categories page is accessible under categories/
+CATEGORIES_URL = "categories/"
+CATEGORIES_SAVE_AS = "categories/index.html"
+
+# Tags page is accessible under tags/
+TAGS_URL = "tags/"
+TAGS_SAVE_AS = "tags/index.html"
+
+# Articles are accessible under post/{yyyy}/{mm}/{dd}/{slug}/
+ARTICLE_URL = ARTICLE_LANG_URL = "post/{date:%Y}/{date:%m}/{date:%d}/{slug}/"
+ARTICLE_SAVE_AS = ARTICLE_LANG_SAVE_AS = ARTICLE_URL + "index.html"
+
+# Pages are accessible under {path of the source file}/{slug}/
+PAGE_URL = PAGE_LANG_URL = "{directory_path}/{slug}/"
+PAGE_SAVE_AS = PAGE_LANG_SAVE_AS = PAGE_URL + "index.html"
+
+# Authors are accessible under author/{slug}/
+AUTHOR_URL = "author/{slug}/"
+AUTHOR_SAVE_AS = AUTHOR_URL + "index.html"
+
+# Categories are accessible under category/{slug}/
+CATEGORY_URL = "category/{slug}/"
+CATEGORY_SAVE_AS = CATEGORY_URL + "index.html"
+
+# Categories are accessible under tag/{slug}/
+TAG_URL = "tag/{slug}/"
+TAG_SAVE_AS = TAG_URL + "index.html"
 
 ## I18N/L10N SETTINGS ##
 
@@ -24,7 +75,9 @@ TIMEZONE = 'America/Montreal'
 I18N_TEMPLATES_LANG = "fr"
 DEFAULT_LANG = "fr"
 I18N_SUBSITES = {
-    "en": dict()
+    "en": {
+        "LOCALE": "en_US"
+    }
 }
 LOCALE = "fr_CA"
 
@@ -42,7 +95,24 @@ AUTHOR_FEED_RSS = None
 # Source files
 PATH = "content"
 
-DEFAULT_PAGINATION = 10
+# We put our articles in a subdirectory, for a clearer separation
+ARTICLES_PATHS = [
+    "articles"
+]
+
+# Source static files
+STATIC_PATHS = [
+    "images",
+]
+
+DEFAULT_ORPHANS = 3  # Minimum number of articles for the last page
+DEFAULT_PAGINATION = 12  # Number of articles per page
+
+# Access subsequent pages via <url>/<page number>/
+PAGINATION_PATTERNS = (
+    (1, "{base_name}/", "{save_as}"),
+    (2, "{base_name}/{number}/", "{base_name}/{number}/index.html"),
+)
 
 # Lander page only
 DIRECT_TEMPLATES = []
@@ -58,4 +128,11 @@ SOCIAL = (
 THEME = "theme"
 DISPLAY_PAGES_ON_MENU = False
 DISPLAY_CATEGORIES_ON_MENU = False
-LANDER_DISPLAY_SOCIALS_IN_FOOTER = True
+DISPLAY_SOCIALS_IN_FOOTER = True
+
+## TAILWIND CSS ##
+
+TAILWINDCSS_ENABLE = True
+TAILWINDCSS_VERSION = "v4.1.18"
+TAILWINDCSS_MINIFY = False
+TAILWINDCSS_INPUT_FILES = [f"{THEME}/static/css/style.css"]
